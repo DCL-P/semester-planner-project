17f6465f07d8d731731165feff245ecca2ed3ebb
Runix <164242635+DCL-P@users.noreply.github.com>
2026-09-09 15:49:12 +0200

    added the frontend server (node)
    made a small node server that loads the html homepage file
    
    And made an API call to test the connection between the frontend and backend (they can connect :) )

--- frontend/node/server.js
+++ frontend/node/server.js
@@ -1,1 +1,29 @@
 // DEV NOTE deze folder voor nu nog niet nodig dit is voor de connectie met de backend op te zetten
+import http from "node:http";
+
+
+//frontend node server
+const server = http.createServer((req, resp)=>{
+    resp.writeHead(200, {"Content-Type": "../home.html"});
+    resp.end("hELLO FROM LAN WOOOAAAAAHHHHHOOOOOOOOOOO");
+});
+
+server.listen(1500, "0.0.0.0", () => {
+    console.log("listening");
+});
+
+
+
+
+//API connection with backend
+const response = await fetch('http://192.168.1.3:3000', {
+method: 'POST',
+headers: {
+    'Content-Type': 'application/json',
+},
+body: JSON.stringify({
+    message: 'WSG BACKEND',
+}),
+});
+
+console.log(await response.json());
