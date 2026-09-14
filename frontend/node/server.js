5f0acb97bb49355054f6cc0a1824af56be4f829a
Runix <164242635+DCL-P@users.noreply.github.com>
2026-09-14 15:12:29 +0200

    made sure frontend server can load html
    made sure the frontend hosts on localhost instead of local ip
    
    And made sure html files are able to be loaded in

--- frontend/node/server.js
+++ frontend/node/server.js
@@ -1,22 +1,28 @@
 // DEV NOTE deze folder voor nu nog niet nodig dit is voor de connectie met de backend op te zetten
 import http from "node:http";
+import { readFile } from "node:fs/promises";
 
 
 //frontend node server
-const server = http.createServer((req, resp)=>{
-    resp.writeHead(200, {"Content-Type": "../home.html"});
-    resp.end("hELLO FROM LAN WOOOAAAAAHHHHHOOOOOOOOOOO");
+const server = http.createServer(async (req, resp)=>{
+    if (req.url !== "/") {
+        resp.writeHead(404, {"Content-Type": "text/plain; charset=utf-8"});
+        resp.end("Not found");
+        return;
+    }
+
+    const homeHtml = await readFile(new URL("../home.html", import.meta.url));
+    resp.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    resp.end(homeHtml);
 });
 
-server.listen(1500, "0.0.0.0", () => {
+server.listen(1500, () => {
     console.log("listening");
 });
 
 
-
-
 //API connection with backend
-const response = await fetch('http://192.168.1.3:3000', {
+const response = await fetch('http://localhost:3000', {
 method: 'POST',
 headers: {
     'Content-Type': 'application/json',
