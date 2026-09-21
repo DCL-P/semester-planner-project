15597ffeed6a0ddc3d223de7afcebba4a72c79e3
Runix <164242635+DCL-P@users.noreply.github.com>
2026-09-21 16:45:24 +0200

    miscellaneous changes

--- backend/src/feature_user_activities/activities.module.ts
+++ backend/src/feature_user_activities/activities.module.ts
@@ -2,8 +2,11 @@ import { Module } from "@nestjs/common";
 import { ActivitiesController } from "./activities.controller.js";
 import { ActivitiesService } from "./activities.service.js";
 import { ActivitiesRepository } from "./activities.repository.js";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Activity } from "./activities.entities.js";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Activity])],
     controllers: [ActivitiesController],
     providers: [ActivitiesService, ActivitiesRepository]
 })
