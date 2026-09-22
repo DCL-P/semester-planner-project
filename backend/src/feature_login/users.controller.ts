b84e5429c0b260bfe6b0a806ed1dc902ddc60c4d
Runix <164242635+DCL-P@users.noreply.github.com>
2026-09-22 20:08:33 +0200

    made sure fetchedWeeks are parsed in the user controller
    And also made sure the activitiesService is loaded in so that weeks can be fetched through the service

--- backend/src/feature_login/users.controller.ts
+++ backend/src/feature_login/users.controller.ts
@@ -1,22 +1,38 @@
 import { Controller, Get, Param, Body, Post, HttpCode, HttpStatus, Render } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
+import { ActivitiesService } from '../feature_user_activities/activities.service.js';
 import type { CreateUserDto } from './user.repository.js';
 
 @Controller()
 export class AuthController {
   constructor(
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly activitiesService: ActivitiesService
   ) {}
+
+  @Get('/signup')
+  @Render('partials/signup')
+  template(){
+
+  }
 
   @HttpCode(HttpStatus.OK)
   @Post('redirect/planner')
   @Render('partials/planner')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password)
+  async signIn(@Body() signInDto: Record<string, any>) {
+    const user = this.authService.signIn(signInDto.username, signInDto.password);
+
+    const fetchedWeeks = await this.activitiesService.fetchAllWeeks();
+
+    console.log(fetchedWeeks);
+    return {
+        user,
+        fetchedWeeks,
+    };
   }
 
   @HttpCode(HttpStatus.OK)
-  @Post('signup')
+  @Post('/signup')
   @Render('index')
   signUp(@Body() signUpDto: CreateUserDto) {
     return this.authService.signUp(signUpDto);
